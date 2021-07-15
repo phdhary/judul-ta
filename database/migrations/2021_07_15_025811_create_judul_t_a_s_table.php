49cf990d450f646f583f76ef6f5cea48926922fa
Hary <phdhary@gmail.com>
2021-07-15 10:22:48 +0700

    buat model&table judulta,kategori,mahasiswa

---
+++ database/migrations/2021_07_15_025811_create_judul_t_a_s_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJudulTASTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('judul_t_a_s', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_id');
+            $table->foreignId('mahasiswa_id');
+            $table->string('nama_judul',255)->default('');
+            $table->longText('deskripsi')->default(null);
+            $table->string('nama_dosen')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('judul_t_a_s');
+    }
+}
