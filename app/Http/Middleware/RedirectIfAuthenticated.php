b7f733fa38678ba17bc0ff78895dde9a0482628e
Hary <phdhary@gmail.com>
2021-07-15 17:07:39 +0700

    fix parameter Auth Guard

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,10 +26,10 @@ class RedirectIfAuthenticated
             //     return redirect(RouteServiceProvider::HOME);
             // }
 
-            if(Auth::guard('$guard')->check() && Auth::user()->role == 1){
+            if(Auth::guard($guard)->check() && Auth::user()->role == 1){
                 return redirect()->route('admin.dashboard');
             }
-            elseif(Auth::guard('$guard')->check() && Auth::user()->role == 2){
+            elseif(Auth::guard($guard)->check() && Auth::user()->role == 2){
                 return redirect()->route('user.dashboard');
             }
         }
